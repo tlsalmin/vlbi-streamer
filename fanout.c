da6a5ca9461ce8f418b70bae58b0c7757630d27c
Tomi Salminen <tlsalmin@gmail.com>
2012-02-23 11:55:30 +0200

    Working fanout with packet_mmap. 20% util per core

--- fanout.c
+++ fanout.c
@@ -1,3 +1,7 @@
+#define THREADED
+//#define OUTPUT
+#define MMAP_TECH
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,6 +11,10 @@
 #include <sys/wait.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#ifdef MMAP_TECH
+#include <sys/mman.h>
+#include <sys/poll.h>
+#endif
 
 #include <unistd.h>
 
@@ -19,11 +27,11 @@
 static const char *device_name;
 static int fanout_type;
 static int fanout_id;
-#define THREADED
-#define OUTPUT
+#ifdef MMAP_TECH
+//volatile struct tpacket_hdr * ps_header_start;
 #define RING_BLOCKSIZE 16384
 #define RING_FRAME_SIZE 8192
-#define RING_BLOCK_NR 16
+#define RING_BLOCK_NR 512
 /*
 #define RING_BLOCKSIZE 4096
 #define RING_BLOCK_NR 4
@@ -31,6 +39,7 @@ static int fanout_id;
 */
 
 #define RING_FRAME_NR  RING_BLOCKSIZE / RING_FRAME_SIZE * RING_BLOCK_NR
+#endif
 
 #ifndef PACKET_FANOUT
 #define PACKET_FANOUT		18
@@ -42,14 +51,15 @@ static int fanout_id;
 #else
 #define THREADS 6
 #endif
-#define PACKET_NUM 100000000
-#define BUFSIZE 2^16
+#define PACKET_NUM 1000000
+#define BUFSIZE 65536
 //From netcat
 #define SA struct sockaddr	/* socket overgeneralization braindeath */
 #define SAI struct sockaddr_in	/* ... whoever came up with this model */
 #define IA struct in_addr	/* ... should be taken out and shot, */
 
 
+//This is a pretty silly way to do this
 static int setup_socket(void)
 {
   int err, fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IP));
@@ -57,8 +67,6 @@ static int setup_socket(void)
   //int err, fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_IP));
   struct sockaddr_ll ll;
   struct ifreq ifr;
-  //for MMAP-buffer
-  struct tpacket_req req;
   int fanout_arg;
   int o_rcvbuf;
 
@@ -77,7 +85,7 @@ static int setup_socket(void)
 
   memset(&ll, 0, sizeof(ll));
   ll.sll_family = AF_PACKET;
-  ll.sll_protocol = ETH_P_ALL;
+  //ll.sll_protocol = ETH_P_ALL;
   ll.sll_ifindex = ifr.ifr_ifindex;
   err = bind(fd, (struct sockaddr *) &ll, sizeof(ll));
   if (err < 0) {
@@ -93,7 +101,30 @@ static int setup_socket(void)
     perror("setsockopt");
     return EXIT_FAILURE;
   }
-#endif
+#endif //THREADED
+  return fd;
+}
+  /*
+     o_rcvbuf = 131071;
+     err = setsockopt(fd, SOL_PACKET, SO_RCVBUF, &o_rcvbuf, sizeof(o_rcvbuf));
+     if (err) {
+     perror("RCVBUF augment failed");
+     return EXIT_FAILURE;
+     }
+     */
+
+
+static void fanout_thread(void)
+{
+  int fd = setup_socket();
+  int limit = PACKET_NUM;
+  //int x = sizeof(SA);
+  //SAI * remend =NULL;
+  //Changed to polling for mmap
+#ifdef MMAP_TECH
+  uint64_t total_captured = 0;
+  int err;
+  struct tpacket_hdr * ps_header_start;
   /*
   req.tp_block_size= 4096;
     req.tp_frame_size= 2048;
@@ -101,57 +132,100 @@ static int setup_socket(void)
     req.tp_frame_nr  = 8;
   //Set a ringbuffer for mmap
   */
+  //for MMAP_TECH-buffer
+  struct tpacket_req req;
+
   req.tp_block_size = RING_BLOCKSIZE;
   req.tp_frame_size = RING_FRAME_SIZE;
   req.tp_block_nr = RING_BLOCK_NR;
   req.tp_frame_nr = RING_FRAME_NR;
   err = setsockopt(fd, SOL_PACKET, PACKET_RX_RING, (void*) &req, sizeof(req));
-  //err = setsockopt(fd, SOL_PACKET, PACKET_RX_RING, (void *) &req, sizeof(req));
   if (err) {
     perror("PACKET_RX_RING failed");
-    return EXIT_FAILURE;
+    //return EXIT_FAILURE;
   }
-  /*
-  o_rcvbuf = 131071;
-  err = setsockopt(fd, SOL_PACKET, SO_RCVBUF, &o_rcvbuf, sizeof(o_rcvbuf));
-  if (err) {
-    perror("RCVBUF augment failed");
-    return EXIT_FAILURE;
+
+  //MMap the packet ring
+  //TODO: Remember to close ring
+  ps_header_start = mmap(0, RING_BLOCKSIZE*RING_BLOCK_NR, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+  if (!ps_header_start)
+  {
+    perror("mmap");
+    //return EXIT_FAILURE;
   }
-  */
 
-  return fd;
-}
+  struct pollfd pfd;
+  struct tpacket_hdr *header;
+  uint64_t incomplete = 0;
+  uint64_t dropped = 0;
+  uint64_t i;
+  pfd.fd = fd;
+  pfd.revents = 0;
+  //POLLRDNORM specced as == POLLIN and doesn't work
+  //pfd.events = POLLIN|POLLRDNORM|POLLERR;
+  pfd.events = POLLIN|POLLERR;
 
-static void fanout_thread(void)
-{
-  int fd = setup_socket();
-  int limit = PACKET_NUM;
-  int x = sizeof(SA);
-  SAI * remend =NULL;
+  header = (void *) ps_header_start;
+
+#endif //MMAP_TECH
 
   if (fd < 0)
     exit(fd);
 
-  while (limit-- > 0) {
-    char buf[1600];
+  while (limit > 0) {
     int err;
+#ifdef MMAP_TECH
+    while(!(header->tp_status & TP_STATUS_USER)){
+      //1 file descriptor for infinte time
+      err = poll(&pfd, 1, -1);
+    }
+    limit--;
+    //TODO:Packet processing
+    
+    if (header->tp_status & TP_STATUS_COPY)
+      incomplete++;
+    else if (header->tp_status & TP_STATUS_LOSING)
+      dropped++;
+    else
+      total_captured += header->tp_len;
+
+    //Release frame back to kernel use
+    header->tp_status = 0;
+
+    //Update header point
+    //header = (header + 1) & ((struct tpacket_hdr *)(RING_FRAME_NR -1 ));
+    if(i>=RING_FRAME_NR-1)
+      i = 0;
+    else 
+      i++;
+    header = (void *) ps_header_start + i * RING_FRAME_SIZE;
+    //header = ((void *)(header + 1)) & ((void*)(RING_FRAME_NR -1));
+
+#else 
+    char buf[1600];
+    //fprintf(stdout, "STATUS: (%d) \n", getpid());
+    err = recv(fd, buf, BUFSIZE, 0);
+#endif
+    if(err < 0){
+      perror("poll or read");
+      //TODO: Handle error
+      //break;
+    }
 
     //err = read(fd, buf, sizeof(buf));
     //err = recv(fd, buf, BUFSIZE, MSG_WAITALL);
-    err = recv(fd, buf, BUFSIZE, 0);
-    if (err < 0) {
-      perror("read");
-      exit(EXIT_FAILURE);
-    }
 #ifdef OUTPUT
-    if ((limit % 10) == 0)
+    if ((limit % 1000) == 0)
       fprintf(stdout, "(%d) \n", getpid());
 #endif
   }
 
   fprintf(stdout, "%d: Received %d packets\n", getpid(), PACKET_NUM);
 
+#ifdef MMAP_TECH
+  fprintf(stdout, "Captured: %u bytes, with %u dropped and %u incomplete\n", total_captured, dropped, incomplete);
+  munmap(header, RING_BLOCKSIZE*RING_BLOCK_NR);
+#endif
   close(fd);
   exit(0);
 }
@@ -179,6 +253,7 @@ int main(int argc, char **argp)
   fanout_id = getpid() & 0xffff;
 
   for (i = 0; i < THREADS; i++) {
+#ifdef THREADED
     pid_t pid = fork();
 
     switch (pid) {
@@ -189,6 +264,9 @@ int main(int argc, char **argp)
 	perror("fork");
 	exit(EXIT_FAILURE);
     }
+#else	
+    fanout_thread();
+#endif //THREADED
   }
 
   for (i = 0; i < THREADS; i++) {
