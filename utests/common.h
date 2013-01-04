c4c8bc6aa5372bf6f12a8a6124cd01e7c54893b1
Tomi Salminen <tlsalmin@gmail.com>
2013-01-04 12:24:32 +0200

    forgot common for unit tests

---
+++ utests/common.h
@@ -0,0 +1,5 @@
+#ifndef COMMON_UTEST_H
+#define COMMON_UTEST_H
+#define TEST_START(name) fprintf(stdout, "TEST_START: " #name "\n")
+#define TEST_END(name) fprintf(stdout, "TEST_END: " #name "\n")
+#endif
